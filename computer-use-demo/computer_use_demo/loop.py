d23b5d0d42ea5958737d2d249a103b936f6e9def
shota hayashida <shota.hayashida@ariseanalytics.com>
2025-11-04 09:15:41 +0900

    update prompt to directory click URLs at the document

--- computer-use-demo/computer_use_demo/loop.py
+++ computer-use-demo/computer_use_demo/loop.py
@@ -86,6 +86,7 @@ SYSTEM_PROMPT = f"""<SYSTEM_CAPABILITY>
 <IMPORTANT>
 * When using Firefox, if a startup wizard appears, IGNORE IT.  Do not even click "skip this step".  Instead, click on the address bar where it says "Search or enter address", and enter the appropriate search term or URL there.
 * If the item you are looking at is a pdf, if after taking a single screenshot of the pdf it seems that you want to read the entire document instead of trying to continue to read the pdf from your screenshots + navigation, determine the URL, use curl to download the pdf, install and use pdftotext to convert it to a text file, and then read that text file directly with your str_replace_based_edit_tool.
+* When navigating to a URL mentioned in content, first try to click on the link to navigate. If clicking is not possible or fails, then directly enter the URL into the address bar.
 </IMPORTANT>"""
 
 
